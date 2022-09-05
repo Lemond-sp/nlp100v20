42b969cd28fd5c552e8c45e12d959dd3f8c334e6
Leon Kajikawa <0131reon254@gmail.com>
2022-09-05 21:39:04 +0900

    upload ans05.py

--- ch01/ans05.py
+++ ch01/ans05.py
@@ -2,3 +2,16 @@
 与えられたシーケンス（文字列やリストなど)から
 n-gramを作る関数
 '''
+def n_gram(text,n):
+  return [text[i:i+n] for i in range(len(text) - n + 1)]
+
+str = "I am an NLPer"
+#文字bi-gram
+print(n_gram(str,2))
+str = str.split()
+#単語bi-gram
+print(*n_gram(str,2))
+'''
+['I ', ' a', 'am', 'm ', ' a', 'an', 'n ', ' N', 'NL', 'LP', 'Pe', 'er']
+['I', 'am'] ['am', 'an'] ['an', 'NLPer']
+'''
