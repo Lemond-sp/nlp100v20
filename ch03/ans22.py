312e72e3d98d8f01d77c0a3bdd30e591abd278ba
Leon Kajikawa <0131reon254@gmail.com>
2022-09-12 19:18:43 +0900

    update

--- ch03/ans22.py
+++ ch03/ans22.py
@@ -5,6 +5,7 @@ df = pd.read_json('jawiki-country.json',lines=True)
 uk = df.query('title == "イギリス"')['text'].values[0]
 uk_list = uk.split('\n')
 ans = list(filter(lambda x:'[Category:' in x,uk_list))
+##ans = [re.sub('\[\[Category:|\|\*|\]\]','',a) for a in ans]
 ans = [a.replace('[[Category:','').replace('|*','').replace(']]','') for a in ans]
 print(ans)
 
