15aa74c7451446d70ee4c8a2e7724ec7f2153ecf
Leon Kajikawa <0131reon254@gmail.com>
2022-09-12 19:02:40 +0900

    update ans21

--- ch03/ans21.py
+++ ch03/ans21.py
@@ -8,7 +8,11 @@ import re
 
 df = pd.read_json('jawiki-country.json',lines=True)
 uk = df.query('title == "イギリス"')['text'].values[0]
-
+uk_list = re.findall(r'^(.*\[\[Category:.*\]\].*)$',uk,re.MULTILINE+re.VERBOSE)
+print(uk_list)
+#re.VERBOSE:パターン記述の際、改行して見やすくできる
+'''
 uk_list = uk.split('\n')
 ans = list(filter(lambda x:'[Category:' in x,uk_list))
 print(ans)
+'''
