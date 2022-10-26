2e0388ea395c867f84cfdae6ffd0c53a734a6e7a
Leon Kajikawa <0131reon254@gmail.com>
2022-10-26 10:12:13 +0900

    test

--- ch05/test41.py
+++ ch05/test41.py
@@ -7,6 +7,14 @@ class Car():
 a = []
 engine = 100
 tire = "huga"
-a.append(Car(engine,tire)) #ここの挙動
+a.append(Car(engine,tire)) # huga 100
+
+# リストとして扱う
+engine = [120,130]
+tire = ["hu","ga"]
+# 一つのインスタンス変数として扱われる
+a.append(Car(engine,tire))
 for i,car in enumerate(a):
-  print(car.tire,car.engine) # huga 100
+  print(car.engine,car.tire) 
+  # huga 100 (１つ目)
+  # ['hu', 'ga'] [120, 130] (２つ目)
