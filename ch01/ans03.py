c95e9ca949385bb92b8f47b5c438cea0b228cb17
Leon Kajikawa <0131reon254@gmail.com>
2022-12-05 13:42:21 +0900

    add comment

--- ch01/ans03.py
+++ ch01/ans03.py
@@ -2,7 +2,7 @@ import re
 
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 print(str)
-str = re.sub("[,\.]","",str)
+str = re.sub("[,|.]","",str)
 print(str)
 ans = str.split()
 print(ans)
@@ -14,4 +14,7 @@ print(ans)
 https://docs.python.org/ja/3/library/re.html#re.sub
 re.sub(pattern,replace,string,count=0,flags=0)
 pattern → replace
+
+['Now', 'I', 'need', 'a', 'drink', 'alcoholic', 'of', 'course', 'after', 'the', 'heavy', 'lectures', 'involving', 'quantum', 'mechanics']
+[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
 '''
