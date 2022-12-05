3dd0aea41978283886633be00ba12caf16767c28
Leon Kajikawa <0131reon254@gmail.com>
2022-12-05 13:15:52 +0900

    add comment

--- ch01/ans04.py
+++ ch01/ans04.py
@@ -11,3 +11,8 @@ for i,word in enumerate(str):
   else:
     ans[word[:2]] = i + 1
 print(ans)
+
+'''
+['Hi', 'He', 'Lied', 'Because', 'Boron', 'Could', 'Not', 'Oxidize', 'Fluorine.', 'New', 'Nations', 'Might', 'Also', 'Sign', 'Peace', 'Security', 'Clause.', 'Arthur', 'King', 'Can.']
+{'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10, 'Na': 11, 'Mi': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20}
+'''
