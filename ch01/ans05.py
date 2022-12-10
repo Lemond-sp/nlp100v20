ced2db9d050f988c30e720fcd904046f667b52a7
Leon Kajikawa <0131reon254@gmail.com>
2022-12-10 10:27:35 +0900

    fixed bi-gram

--- ch01/ans05.py
+++ ch01/ans05.py
@@ -6,12 +6,13 @@ def n_gram(text,n):
   return [text[i:i+n] for i in range(len(text) - n + 1)]
 
 sentence = "I am an NLPer"
+char_seq = sentence.replace(' ','')
 #文字bi-gram
-print(n_gram(sentence,2))
+print(n_gram(char_seq,2))
 sentence = sentence.split()
 #単語bi-gram
 print(*n_gram(sentence,2))
 '''
-['I ', ' a', 'am', 'm ', ' a', 'an', 'n ', ' N', 'NL', 'LP', 'Pe', 'er']
+['Ia', 'am', 'ma', 'an', 'nN', 'NL', 'LP', 'Pe', 'er']
 ['I', 'am'] ['am', 'an'] ['an', 'NLPer']
 '''
