6850faaee7b6eb2e6e6b73e568a0effb01f92d97
Leon Kajikawa <0131reon254@gmail.com>
2022-12-10 11:13:46 +0900

    add explanation

--- ch01/ans08.py
+++ ch01/ans08.py
@@ -1,12 +1,15 @@
 def cipher(lst):
   for i,c in enumerate(lst):
+    # 英字 and 小文字
     if lst[i].isalpha() and lst[i].islower():
+      # ord :unicode変換
       lst[i] = 219 - ord(lst[i])
   return lst
 
-chr = 'Ab12'
-lst = list(chr)
-print(cipher(lst))
+ch = 'Ab12'
+lst = list(ch)
+print(f'before :{lst}')
+print(f'cipher :{cipher(lst)}')
 '''
 文字列は不変 immutable
 リストに変換すると、更新可能
