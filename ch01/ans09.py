1905ef71cf70881533b3256c8012b28fe4c5c1b8
Leon Kajikawa <0131reon254@gmail.com>
2022-12-10 11:19:59 +0900

    add seed42

--- ch01/ans09.py
+++ ch01/ans09.py
@@ -1,8 +1,11 @@
 import random
+random.seed(42)
+
 sentence = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
-sentence = sentence.split()
+sent_list = sentence.split()
 ans = []
-for word in sentence:
+
+for word in sent_list:
   if len(word) > 4:
     #先頭と末尾の文字以外の取り出し・shuffleはリスト化
     mid = list(word[1:-1])
