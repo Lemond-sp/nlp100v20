a657fe161dd2fd0c0fd907a6cac6627708a3658d
Leon Kajikawa <0131reon254@gmail.com>
2022-12-10 10:49:17 +0900

    fixed output

--- ch01/ans06.py
+++ ch01/ans06.py
@@ -9,6 +9,19 @@ Y = set(n_gram(str2,2))
 union = X | Y
 intersection = X & Y
 difference = X - Y
-print(union,intersection,difference)
-print('se' in X)
-print('se' in Y)
+print(f'X :{X} Y :{Y}\n')
+print(f'X | Y :{union}\nX & Y :{intersection}\nX - Y :{difference}\n')
+
+print('se in X' if 'se' in X else 'se not in X')
+print('se in Y' if 'se' in Y else 'se not in Y')
+
+'''
+X :{'pa', 'se', 'ad', 'ar', 'di', 'is', 'ap', 'ra'} Y :{'pa', 'ar', 'ra', 'ag', 'ap', 'ph', 'gr'}
+
+X | Y :{'pa', 'se', 'ad', 'ar', 'di', 'ag', 'is', 'ap', 'ph', 'gr', 'ra'}
+X & Y :{'pa', 'ar', 'ra', 'ap'}
+X - Y :{'se', 'is', 'di', 'ad'}
+
+se in X
+se not in Y
+'''
