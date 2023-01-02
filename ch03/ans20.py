3f5b93981675b527277733300dae982d1b3882f8
Leon Kajikawa <0131reon254@gmail.com>
2023-01-02 14:59:00 +0900

    fixed

--- ch03/ans20.py
+++ ch03/ans20.py
@@ -1,17 +1,54 @@
+'''
+jawaki-country.json
+title : 記事名(国名)
+text  : 記事本文
+
+jsonl : 1レコードがjsonになっているファイル
+
+'''
+
 #jsonファイル読み込み
 import pandas as pd
 import json
 import re
-
-df = pd.read_json('jawiki-country.json',lines=True)
+# json to daraframe
+df = pd.read_json('jawiki-country.json',orient='records',lines=True)
 #titleがイギリスの記事は1つのみ
 uk = df.query('title == "イギリス"')['text'].values[0]
 print(uk)
 '''
+read_json
+・orient :DataFrameにあった形式でjsonを読み込むため
+  ・行ごとのリスト構造 :orient = 'records'
+  ・列ごとのリスト構造 :orient = 'columns'
+  ・index、columns、dataで明示的に分かれている場合 :orient = 'split'
+
 lines:１行ごとに処理
 query(条件式)
 valuesで値を取り出し
 
+別解
+1 : 直接指定
 uk = df[df['title'] == "イギリス"]['text'].values[0]でも
+
+2 :gzip
+with gzip.open('~.gz') as f:
+  for line in f:
+gzファイルのままでもロード可
+
+3 :jsonモジュール
+with open('~.json') as f:
+  data = json.load(f)
+>>>json.decoder.JSONDecodeError: Extra data: line 2 column 1
+
+jsonlであるため一行ずつ読み込む
+
+with open('jawiki-country.json', encoding='utf-8') as f:
+    for line in f:
+        data = json.loads(line)
+        if data['title'] == 'イギリス':
+            print(data['text'])
+
+https://www.dmysd.net/blog/archives/52#orient-records
 
 '''
--- ch03/ans21.py
+++ ch03/ans21.py
@@ -8,10 +8,19 @@ import re
 
 df = pd.read_json('jawiki-country.json',lines=True)
 uk = df.query('title == "イギリス"')['text'].values[0]
-uk_list = re.findall(r'^(.*\[\[Category:.*\]\].*)$',uk,re.MULTILINE+re.VERBOSE)
+# findall :マッチした「文字列のリスト」を返す
+uk_list = re.findall(r'^(\[\[Category:.*\]\])$',uk,re.MULTILINE+re.VERBOSE)
 print(uk_list)
-#re.VERBOSE:パターン記述の際、改行して見やすくできる
 '''
+
+re.VERBOSE : パターン記述の際、改行して見やすくできる
+re.MULTILINE : 通常'^'は文字列の先頭でのみマッチするが
+              追加で各行の先頭(各開業の直後)でマッチできるようにする
+              '$'も同様である。
+'^' : 文字列の先頭(re.MULTILINEを追加すれば、各行の先頭も)
+'$' : 文字列の末尾(re.MULTILINEを追加すれば、各行の末尾も)
+
+1 : 別解
 uk_list = uk.split('\n')
 ans = list(filter(lambda x:'[Category:' in x,uk_list))
 print(ans)
