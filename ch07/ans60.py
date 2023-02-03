192db1e0247230617e42305f2368b5317bfaeb69
Leon Kajikawa <0131reon254@gmail.com>
2023-02-03 23:12:41 +0900

    60

--- ch07/ans60.py
+++ ch07/ans60.py
@@ -4,10 +4,18 @@ import gensim
 from gensim.models import KeyedVectors
 
 def main():
-  
+
   model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
   print(model['United_States'])
   print(model['United_States'].shape) # (300,)
 
 if __name__ == "__main__":
   main()
+
+'''
+1.save_word2vec_formatで保存したモデル
+KeyedVectors.load_word2vec_format で読み込み
+
+2.saveで保存したモデル
+Word2Vec.load で読み込み
+'''
