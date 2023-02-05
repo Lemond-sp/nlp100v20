6cda9133bff4719c6ed9d55900a908350af6f11e
Leon Kajikawa <0131reon254@gmail.com>
2023-02-05 16:48:38 +0900

    fixed

--- ch04/ans30.py
+++ ch04/ans30.py
@@ -1,5 +1,6 @@
 '''
 mecab -o ./neko.txt.mecab ./neko.txt
+-o : --output(出力ファイルの指定)
 
 解析結果を読み込むプログラムの実装
 各形態素
@@ -11,37 +12,43 @@ mecabの出力フォーマット
 '''
 import re
 #指定された要素を全て削除(したものを別リストに格納)
-with open('./neko.txt.mecab','r') as f:
-  text_dict = []
-  sentence_dict = []
-  # f.read:改行も含めた文字列
-  # f.readlines:改行コードを区切って１行ごとに分解されたリスト
-  for line in f.readlines():
-    if line == '\n':
-      continue
-    # １文
-    elif line != 'EOS\n':
-      node = line.split('\t')
-      # surfaceが空白の場合除外
-      if node[0] == '':
+def main():
+    
+  with open('./neko.txt.mecab','r') as f:
+    text_dict = []
+    sentence_dict = []
+    # f.read:改行も含めた文字列
+    # f.readlines:改行コードを区切って１行ごとに分解されたリスト
+    for line in f.readlines():
+      if line == '\n':
         continue
-      # surface 以外はnode[1]
-      feature = node[1].split(',')
-      word_dict = {
-        "surface":node[0],
-        "base":feature[6],
-        "pos":feature[0],
-        "pos1":feature[1]
-      }
-      sentence_dict.append(word_dict)
-    # 追加
-    # 追加文が無い場合は除外(elseだと、つまり無い場合も追加したら9210→9964)
-    elif len(sentence_dict) != 0:
-      text_dict.append(sentence_dict)
-      sentence_dict = []
+      # １文
+      elif line != 'EOS\n':
+        node = line.split('\t')
+        # surfaceが空白の場合除外
+        if node[0] == '':
+          continue
+        # surface 以外はnode[1]
+        feature = node[1].split(',')
+        word_dict = {
+          "surface":node[0],
+          "base":feature[6],
+          "pos":feature[0],
+          "pos1":feature[1]
+        }
+        sentence_dict.append(word_dict)
+      # 追加
+      # 追加文が無い場合は除外(elseだと、つまり無い場合も追加したら9210→9964)
+      elif len(sentence_dict) != 0:
+        text_dict.append(sentence_dict)
+        sentence_dict = []
 
-  print(len(text_dict))
-  for i in text_dict[:5]:
-    for j in i:
-      print(j)
-    print(' ')
+    print(len(text_dict))
+    for i in text_dict[:5]: # 5個分の文章
+      n += 1
+      for j in i:
+        print(j)
+      print(' ')
+
+if __name__ == "__main__":
+    main()
