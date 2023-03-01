1225ec863cd9833cfad7c9eab17473a067bcc3eb
Leon Kajikawa <0131reon254@gmail.com>
2023-03-01 21:05:25 +0900

    67

---
+++ ch07/ans67.py
@@ -0,0 +1,15 @@
+"""
+国名に関する単語ベクトルを抽出し
+k-meansクラスタリングをクラスタ数k=5として実行せよ
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import gensim
+from gensim.models import KeyedVectors
+
+def main():
+    
+if __name__ == "__main__":
+  main()
