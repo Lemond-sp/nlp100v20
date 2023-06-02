7ba9a5ab7fd3bb64b00c1a9105569bca77e8c7a6
Leon Kajikawa <0131reon254@gmail.com>
2023-06-03 02:12:48 +0900

    fixed

--- ch08/ans71.py
+++ ch08/ans71.py
@@ -2,6 +2,7 @@
 import numpy as np
 import torch
 from torch import nn
+from utils import Net
 
 def main():
     contents_path = 'contents/features/bi-data/train_vec.npy'
@@ -9,29 +10,32 @@ def main():
     x_train = x_train.tolist()
     x_train = np.array(x_train)
     x_train = torch.tensor(x_train,requires_grad=True).float()
+    x_train.retain_grad()
     # 重み行列
     feature_num = 4
-    W = torch.randn(x_train.shape[1],feature_num) # 300,4
-    xW = torch.mm(x_train,W)
+    net = Net(x_train.shape[1],feature_num)
+    x = net(x_train)
 
     s = nn.Softmax(dim=1)
-    ans = s(xW)
+    ans = s(x)
     print(ans,ans[0:4][:],sep='\n') # 事例1~4までの出力結果も
 
-    torch.save(ans,'contents/output/ans71.pt')
+    torch.save(net.state_dict(),'contents/output/ans71-model.pt')
+    torch.save(x_train,'contents/output/x_train.pt')
+    
 if __name__ == "__main__":
     main()
 
 '''
-tensor([[0.0363, 0.0823, 0.2563, 0.6251],
-        [0.1019, 0.0511, 0.5000, 0.3469],
-        [0.2088, 0.0806, 0.4425, 0.2681],
+tensor([[0.2512, 0.2260, 0.2569, 0.2659],
+        [0.2641, 0.2403, 0.2490, 0.2467],
+        [0.2610, 0.2348, 0.2573, 0.2469],
         ...,
-        [0.3278, 0.3156, 0.0281, 0.3285],
-        [0.0196, 0.0104, 0.6584, 0.3115],
-        [0.0518, 0.0484, 0.8597, 0.0400]], grad_fn=<SoftmaxBackward0>)
-tensor([[0.0363, 0.0823, 0.2563, 0.6251],
-        [0.1019, 0.0511, 0.5000, 0.3469],
-        [0.2088, 0.0806, 0.4425, 0.2681],
-        [0.8974, 0.0124, 0.0588, 0.0314]], grad_fn=<SliceBackward0>)
+        [0.2573, 0.2164, 0.2576, 0.2686],
+        [0.2678, 0.2330, 0.2488, 0.2504],
+        [0.2545, 0.2391, 0.2535, 0.2529]], grad_fn=<SoftmaxBackward0>)
+tensor([[0.2512, 0.2260, 0.2569, 0.2659],
+        [0.2641, 0.2403, 0.2490, 0.2467],
+        [0.2610, 0.2348, 0.2573, 0.2469],
+        [0.2780, 0.2365, 0.2521, 0.2334]], grad_fn=<SliceBackward0>)
 '''
--- ch08/ans72.py
+++ ch08/ans72.py
@@ -5,32 +5,44 @@ import os
 import numpy as np
 import torch
 from torch import nn
+from utils import Net
 
 def main():
+    contents_path = 'contents'
+    x_train = torch.load(os.path.join(contents_path,'output/x_train.pt'))
+    x_train.retain_grad() # 逆伝搬時に末端以外のテンソルの微分係数の記録を可能にする
+    feature_num = 4
+    
+    # # 損失
+    criterion = nn.CrossEntropyLoss()
     contents_path = 'contents/output'
-    x_train = torch.load(os.path.join(contents_path,'ans71.pt')) # torch.Size([10672, 4])
-    y_train = np.load(os.path.join(contents_path,'../features/bi-data/train_label.npy')) # float64,(N,)
-    y_train = torch.LongTensor(y_train) # 64bit符号付き整数
+    t = np.load(os.path.join(contents_path,'../features/bi-data/train_label.npy')) # float64,(N,)
+    t = torch.LongTensor(t) # 64bit符号付き整数
+    t1 = t[0]
+    t2 = t[0:4]
+    net1 = Net(300,feature_num=feature_num)
+    net1.load_state_dict(torch.load('contents/output/ans71-model.pt'))
+    net2 = Net(300,feature_num=feature_num)
+    net2.load_state_dict(torch.load('contents/output/ans71-model.pt'))
     
-    # 損失
-    loss = nn.CrossEntropyLoss()
-    x1 = x_train[0][:] # スライスすると，x1が末端変数でない(grad_fn=SliceBackward)からgradがnoneになる
+    x1 = x_train[0][:] # スライスすると，x1が末端変数でない(grad_fn=SliceBackward)からgradがnoneになる(retain_gradTrueだとおｋ)
     x2 = x_train[0:4][:] # is_leafで末端のテンソルかどうか確認
-    # 逆伝搬時に末端以外のテンソルの微分係数の記録を可能にする
-    x1.retain_grad()
-    x2.retain_grad()
+
+    y1 = net1(x1)
+    y2 = net2(x2)
+    loss1 = criterion(y1, t1)
+    loss2 = criterion(y2, t2)
+    net1.zero_grad()
+    loss1.backward()
+    loss2.backward()
+    print('----事例x1----')
+    print('損失 :', loss1.item())
+    print('勾配')
+    print(net1.fc.weight.grad)
+    print('----事例集合1,2,3,4----')
+    print('損失 :', loss2.item())
+    print('勾配')
+    print(net2.fc.weight.grad)
     
-    out1 = loss(x1,y_train[0])
-    out2 = loss(x2,y_train[0:4])
-
-    print(out1,out2)
-    # 行列に対する勾配
-    out1.retain_grad()
-    out2.retain_grad()
-    out1.backward()
-    out2.backward()
-
-    print(x1.grad,x2.grad) # x_train.grad[0][:]でもよかった
-
 if __name__ == "__main__":
     main()
